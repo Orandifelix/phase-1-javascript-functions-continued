b456a10bafae44955ae242fc2c6edb4d3282b51d
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-03-29 15:04:28 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+
+    return (`This Saturday, I want to ${activity}!`)
+
+} 
+function mondayWork(activity="go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(wrapper){
+    switch (wrapper){
+        case "*":
+            return (function (){
+                return `You are ${wrapper}a hard worker${wrapper}!`
+            })
+       case "||":
+       return  (function(){
+        return `You are ${wrapper}a dedicated programmer${wrapper}!`
+       })    
+    }
+}
